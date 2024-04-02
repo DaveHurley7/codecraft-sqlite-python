b7e56e3f28d0412e89707a68321ac2bea890abdd
Dave Hurley <djmhur@gmail.com>
2024-04-02 06:59:09 +0100

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        print(f"database page size: {page_size}")
+        database_file.seek(103)
+        table_amt = int.from_bytes(database_file.read(2), byteorder="big")
+        print(f"database page size: {page_size}\number of tables: {table_amt}")
 else:
     print(f"Invalid command: {command}")
