86916698389ebfba259f35f4fc6f5b337207b2b1
Dave Hurley <djmhur@gmail.com>
2024-04-05 08:27:34 +0100

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -110,10 +110,7 @@ elif command.lower().startswith("select"):
             results = [[r[col_idx] for col_idx in col_idxs] for r in records]
             #print("RESULTS:",results)
             for res in results:
-                last_sep = len(res)-1
-                for i, col in enumerate(res):
-                    print(col,sep="" if i < last_sep else "|", end="")
-                print()
+                print(*res)
         
 else:
     print(f"Invalid command: {command}")
