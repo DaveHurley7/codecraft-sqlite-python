5dc406569bbdafcd461f86ff7663e141291a6bab
Dave Hurley <djmhur@gmail.com>
2024-04-11 22:41:40 +0100

    pass 7th stage

--- app/main.py
+++ app/main.py
@@ -94,6 +94,7 @@ def get_records(start_offset,cells,db_file,tdesc,query_ref):
             if not query_ref.cond.comp(record[query_ref.cond.col]):
                 continue
         records.append(list(record.values()))
+    print("RECORDS:",records)
     return records
 
 if command == ".dbinfo":
