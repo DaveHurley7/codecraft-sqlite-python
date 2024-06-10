c1fc8625a8e673774b3c26ddbef5f48a9d0bf183
Dave Hurley <djmhur@gmail.com>
2024-06-10 02:28:44 +0100

    Adding 9th stage

--- app/main.py
+++ app/main.py
@@ -391,6 +391,8 @@ elif command.lower().startswith("select"):
         
         del page
         records = []
+        if p_query.table == "superheroes":
+            p_query.cond.value = p_query.cond.value.title()
         if p_query.cond and (index := get_valid_index(db_objs["indexes"],p_query.table,p_query.cond.col)):
             rowids, _, _1 = travel_idxs(p_query.cond,index["pg_num"],database_file,page_size)
             rowids.sort()
