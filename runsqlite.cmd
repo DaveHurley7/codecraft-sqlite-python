218a35158c15d66395290613f7a8eec26dbdc35e
Dave Hurley <djmhur@gmail.com>
2024-06-10 03:00:53 +0100

    Adding 9th stage

--- runsqlite.cmd
+++ runsqlite.cmd
@@ -1,4 +1,4 @@
-pipenv run python -m app.main companies.db "SELECT id, name FROM companies WHERE country = 'chad'"
+pipenv run python -m app.main companies.db "SELECT id, name FROM companies WHERE country = 'republic of the congo'" ::chad'"
 ::'north korea'" 
 ::''" 
 ::'eritrea'"
